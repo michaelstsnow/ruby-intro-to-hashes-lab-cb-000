45da190d9152744610006ab7013e2f290f3ca075
michaelstsnow <michael.st.snow@gmail.com>
2018-11-07 12:35:24 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -51,18 +51,10 @@ def monopoly_with_fourth_tier
 		:railroads => {
 			:pieces => 4,
 			:names => {
-				:reading_railroad => {
-					mortgage_value => "100"
-				},
-				:pennsylvania_railroad => {
-					mortgage_value => "100"
-				},
-				:b_and_o_railroad => {
-					mortgage_value => "100"
-				},
-				:shortline => {
-					mortgage_value => "100"
-				}
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
+				:shortline => {}
 			},
 			:rent_in_dollars => {
 				:one_piece_owned => 25,
